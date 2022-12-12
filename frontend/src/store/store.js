6048160c6df34b2aebd2349226ca81cf83184073
Izaiah Lucero <izaiahlucero@utlook.com>
2022-12-11 22:29:40 -0700

    Updated files to get a working starter project.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,5 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
-import truck from "./truck.js";
-import menus from "./menus.js";
 import auth from "./auth.js";
 
-const reducer = combineReducers({truck,menus,auth})
+const reducer = combineReducers({auth})
 export default configureStore({reducer})
--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -1,10 +1,25 @@
 import React, {useEffect} from "react"
+import { Col, Container, Row } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
 
 
 export function Home() {
-    return (
-        <>
 
+  const dispatch = useDispatch()
+  const initialEffects = () => {
+
+  }
+  useEffect(initialEffects, [dispatch])
+
+  return (
+        <>
+          <Container>
+            <Row>
+              <Col>
+                <h1>Test</h1>
+              </Col>
+            </Row>
+          </Container>
         </>
     )
 }
